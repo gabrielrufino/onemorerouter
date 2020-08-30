c9a9b698968cce8e16650b0a7539edb0a7d85e13
Gabriel Rufino <contato@gabrielrufino.com>
2020-08-30 16:43:20 -0300

    Initial class Server

---
+++ src/Server.js
@@ -0,0 +1,81 @@
+const http = require('http')
+
+const Request = require('./Request')
+const Response = require('./Response')
+
+class Server {
+  constructor (props) {
+    const { port } = props
+
+    this._port = port
+    this._server = http.createServer()
+    this._routes = {}
+
+    this._server.on('request', (httpRequest, httpResponse) => {
+      const { method, url } = request
+
+      const request = new Request(httpRequest)
+      const response = new Response(httpResponse)
+
+      this._routes[url] &&
+      this._router[url][method] &&
+      this._router[url][method](request, response)
+    })
+  }
+
+  get (url, callback) {
+    if (this._routes[url]) {
+      this._routes[url].GET = callback
+    } else {
+      this.routes[url] = {
+        GET: callback
+      }
+    }
+  }
+
+  post (url, callback) {
+    if (this._routes[url]) {
+      this._routes[url].POST = callback
+    } else {
+      this.routes[url] = {
+        POST: callback
+      }
+    }
+  }
+
+  patch (url, callback) {
+    if (this._routes[url]) {
+      this._routes[url].PATCH = callback
+    } else {
+      this.routes[url] = {
+        PATCH: callback
+      }
+    }
+  }
+
+  put (url, callback) {
+    if (this._routes[url]) {
+      this._routes[url].PUT = callback
+    } else {
+      this.routes[url] = {
+        PUT: callback
+      }
+    }
+  }
+
+  delete (url, callback) {
+    if (this._routes[url]) {
+      this._routes[url].DELETE = callback
+    } else {
+      this.routes[url] = {
+        DELETE: callback
+      }
+    }
+  }
+
+  listen (callback) {
+    this._server.listen(this._port, callback)
+  }
+}
+
+module.exports = Server
