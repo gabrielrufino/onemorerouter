b001b675e045100dc76b10ca118f52248d6a4542
Gabriel Rufino <contato@gabrielrufino.com>
2020-08-31 22:29:20 -0300

    Parse json data

--- src/Request.js
+++ src/Request.js
@@ -1,17 +1,26 @@
 class Request {
   constructor (httpRequest) {
-    const { headers } = httpRequest
+    const { headers, method } = httpRequest
 
     this.headers = headers
-    this.body = []
 
-    httpRequest
-      .on('data', chunk => {
-        this.body.push(chunk)
-      })
-      .on('end', () => {
-        this.body = Buffer.concat(this.body).toString()
-      })
+    if (['POST', 'PUT', 'PATCH'].includes(method)) {
+      this.body = []
+
+      httpRequest
+        .on('data', chunk => {
+          this.body.push(chunk)
+        })
+        .on('end', () => {
+          this.body = Buffer.concat(this.body).toString()
+
+          const contentType = headers['Content-Type']
+
+          if (contentType === 'application/json') {
+            this.body = JSON.parse(this.body)
+          }
+        })
+    }
   }
 }
 
